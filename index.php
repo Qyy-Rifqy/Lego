1c6d006ad8add856b6741694ab437ae7425064fb
Qyy-Rifqy <116615680+Qyy-Rifqy@users.noreply.github.com>
2022-10-25 15:23:43 +0800

    Landing Page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lego</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/font-awesome.css">
+    <link rel="icon" href="assets/legoo.png">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+/>
+</head>
+<body class="font-sans">
+    <div class="container-2xl mx-auto bg-[#FFCF00] border-b-2 p-1.5 text-center w-full items-center">
+        <nav>
+            <ul  class="flex justify-start items-center text-black">
+                <li>
+                    <img class="w-16 h-auto" src="assets/legoo.png" alt="">
+                </li>
+                <a class="hover:text-[#E3030B] mx-3" href="">Technic</a>
+                <a class="hover:text-[#E3030B] mx-3" href="">Creator</a>
+                <a class="hover:text-[#E3030B] mx-3" href="">Ideas</a>
+                <a class="hover:text-[#E3030B] mx-3" href="">Classic</a>
+            <div class="ml-[850px]">
+                <a class="bg-[#E3030B] text-white px-4 py-1 rounded-lg font-bold" href="">Login</a>
+            </div>
+            </ul>
+        </nav>
+  </div>
